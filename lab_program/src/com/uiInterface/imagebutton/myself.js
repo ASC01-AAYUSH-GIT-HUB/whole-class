a6b0133051dfe70b7bb4715c7445f7e4889b1525
unknown <prasad061203@gmail.com>
2025-07-28 02:15:26 +0530

    my ui path start

---
+++ lab_program/src/com/uiInterface/imagebutton/myself.js
@@ -0,0 +1,6 @@
+// Get the image by its ID and assign a click event
+document.getElementById("ramboButton").addEventListener("click", function() {
+  alert("I am Rambo");
+  document.getElementById("message").innerText= "You clicked the image button!";
+});
+// This code will show an alert when the image button is clicked
